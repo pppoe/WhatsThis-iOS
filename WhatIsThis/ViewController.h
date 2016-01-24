cfbe24e1653fccaca87aefceb386428733b59fc7
pppoe <mr.pppoe@gmail.com>
2016-01-23 20:06:59 -0500

    Initial Commit

---
+++ WhatIsThis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhatIsThis
+//
+//  Created by Haoxiang Li on 1/23/16.
+//  Copyright © 2016 Haoxiang Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WhatIsThis/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WhatIsThis
+//
+//  Created by Haoxiang Li on 1/23/16.
+//  Copyright © 2016 Haoxiang Li. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
